3cc8936ae827ca9ec9162f2035034735db16d648
Chris Jordan <jordanchriss@gmail.com>
2019-03-01 18:56:09 -0500

    sending token to websocket client (original window)

--- neuroglancer_auth/__init__.py
+++ neuroglancer_auth/__init__.py
@@ -1,5 +1,7 @@
 import flask
 from flask_sockets import Sockets
+from flask_session import Session
+
 from .server import mod, ws
 from werkzeug.contrib.fixers import ProxyFix
 __version__ = '0.0.13'
@@ -7,8 +9,9 @@ __version__ = '0.0.13'
 
 def create_app():
     app = flask.Flask(__name__)
-
     app.config.from_object('neuroglancer_auth.config.Config')
+
+    Session(app)
 
     print(app.secret_key)
 
--- neuroglancer_auth/config.py
+++ neuroglancer_auth/config.py
@@ -1,2 +1,7 @@
+import redis
+import os
+
 class Config(object):
 	SECRET_KEY = 'test'
+	SESSION_TYPE = 'redis'
+	SESSION_REDIS = redis.from_url('redis://' + os.environ.get('REDISHOST', 'localhost') + ':' + str(os.environ.get('REDISPORT', 6379)))
