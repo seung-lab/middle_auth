f6d02a60ecf02c2624e6f6205bd4f3ec87930af0
Chris Jordan <jordanchriss@gmail.com>
2019-03-01 16:04:16 -0500

    debug logging

--- run.py
+++ run.py
@@ -3,7 +3,16 @@
 from werkzeug.serving import WSGIRequestHandler
 from neuroglancer_auth import create_app
 import os
+import sys
 
 HOME = os.path.expanduser("~")
 
 application = create_app()
+
+print(os.environ)
+print(sys.argv)
+
+# from gevent import pywsgi
+# from geventwebsocket.handler import WebSocketHandler
+# server = pywsgi.WSGIServer(('', 4000), application, handler_class=WebSocketHandler)
+# server.serve_forever()
