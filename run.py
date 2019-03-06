b2e55a3957d0b55b5538e1bb350313f2875cebe4
Chris Jordan <jordanchriss@gmail.com>
2019-03-06 16:50:04 -0500

    having trouble with basic flask app so rolling back to previous state to make sure my sanity is intact

--- run.py
+++ run.py
@@ -1,26 +1,14 @@
 # Run a test server.
 # import sys
-# from werkzeug.serving import WSGIRequestHandler
-# from neuroglancer_auth import create_app
-# import os
+from werkzeug.serving import WSGIRequestHandler
+from neuroglancer_auth import create_app
+import os
 
-# HOME = os.path.expanduser("~")
+HOME = os.path.expanduser("~")
 
-# application = create_app()
+application = create_app()
 
-# from gevent import pywsgi
-# from geventwebsocket.handler import WebSocketHandler
-# server = pywsgi.WSGIServer(('', 5000), application, handler_class=WebSocketHandler)
-# server.serve_forever()
-
-
-from flask import Flask
-from flask_cors import CORS
-
-application = Flask(__name__)
-cors = CORS(application, resources={r"*": {"origins": "https://developer.mozilla.org"}})
-
-@application.route('/', defaults={'path': ''})
-@application.route('/<path:path>')
-def helloWorld(path):
-	return "Hello, cross-origin-world! " + path
+from gevent import pywsgi
+from geventwebsocket.handler import WebSocketHandler
+server = pywsgi.WSGIServer(('', 5000), application, handler_class=WebSocketHandler)
+server.serve_forever()
