5d0b44a45130f9e3246bd822f477cf26479489d2
Chris Jordan <jordanchriss@gmail.com>
2019-05-03 16:59:15 -0400

    trying to do auth without websockets

--- sample-client/script.js
+++ sample-client/script.js
@@ -1,27 +1,16 @@
 // returns a token to be used with services that use the given auth service
 async function authorize(auth_url) {
-	await fetch(`https://${auth_url}/establish_session?origin=${encodeURI(window.location.origin)}`, {
+	const oauth_uri = await fetch(`https://${auth_url}/authorize?origin=${encodeURI(window.location.origin)}`, {
 		credentials: 'include'
+	}).then((res) => {
+		return res.text();
 	});
 
-	return await new Promise((f, r) => {
-		const socket = new WebSocket(`wss://${auth_url}/authorize`);
+	const auth_popup = window.open(oauth_uri);
 
-		let auth_popup = null;
-
-		socket.onmessage = function (msg) {
-			if (msg.data.startsWith('http')) {
-				auth_popup = window.open(msg.data);
-
-				if (!auth_popup) {
-					alert('Allow popups on this page to authenticate');
-				}
-			} else {
-				auth_popup.close();
-				f(msg.data);
-			}
-		}
-	});
+	if (!auth_popup) {
+		alert('Allow popups on this page to authenticate');
+	}
 }
 
 function parseWWWAuthHeader(headerVal) {
@@ -87,7 +76,10 @@ function reauthenticate(realm) {
 	});
 }
 
-authFetch('https://dev.dynamicannotationframework.com/auth/test').then((res) => {
+// const local_url = 'http://localhost:5000/auth';
+const test_url = 'https://dev.dynamicannotationframework.com/auth/test';
+
+authFetch(test_url).then((res) => {
 	return res.json();
 }).then((user_id) => {
 	alert(`User ID: ${user_id}`);
