4a320676ab1f55b0e9a82ca5e7dac21b3fe213f8
Chris Jordan <jordanchriss@gmail.com>
2019-05-03 16:59:15 -0400

    trying to do auth without websockets

--- neuroglancer_auth/__init__.py
+++ neuroglancer_auth/__init__.py
@@ -1,13 +1,11 @@
 from flask import Flask
-from flask_uwsgi_websocket import GeventWebSocket
 
 app = Flask(__name__)
-sockets = GeventWebSocket(app) # need to monkeypatch before loading a module that imports ssl https://github.com/gevent/gevent/issues/1016
 
 from flask_session import Session
 from flask_cors import CORS
 
-from .server import mod, setup_socket_route
+from .server import mod
 from werkzeug.contrib.fixers import ProxyFix
 __version__ = '0.0.19'
 
@@ -21,9 +19,5 @@ def setup_app():
 
     app.wsgi_app = ProxyFix(app.wsgi_app)
     app.register_blueprint(mod)
-
-    ws = setup_socket_route(app)
-
-    sockets.register_blueprint(ws)
 
     return app
