d71d39fa5c4968553924f911888b29eaacfbf386
Chris Jordan <jordanchriss@gmail.com>
2019-05-16 14:53:35 -0400

    added postgres database to save users and roles

--- neuroglancer_auth/config.py
+++ neuroglancer_auth/config.py
@@ -5,3 +5,5 @@ class Config(object):
 	SECRET_KEY = 'test'
 	SESSION_TYPE = 'redis'
 	SESSION_REDIS = redis.from_url('redis://' + os.environ.get('REDISHOST', 'localhost') + ':' + str(os.environ.get('REDISPORT', 6379)))
+	SQLALCHEMY_DATABASE_URI = "postgresql://postgres:uhkbjFPhnmtriotd@10.220.176.4/test1"
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
