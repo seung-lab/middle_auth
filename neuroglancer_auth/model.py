5ce14d7394b1e33452176ad3c3f3df47092a46f5
Chris Jordan <jordanchriss@gmail.com>
2019-06-05 15:02:27 -0400

    test

--- neuroglancer_auth/model.py
+++ neuroglancer_auth/model.py
@@ -11,6 +11,9 @@ class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
 
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
 class UserRole(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column('user_id', db.Integer, db.ForeignKey("user.id"), nullable=False)
