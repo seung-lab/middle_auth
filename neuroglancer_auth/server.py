c2f38736b85b541f8d15383039b04a8fbd52e110
Chris Jordan <jordanchriss@gmail.com>
2019-06-11 18:02:23 -0400

    moved most of the data logic out of server.js and into the model classes

--- neuroglancer_auth/server.py
+++ neuroglancer_auth/server.py
@@ -6,7 +6,7 @@ import urllib
 import uuid
 import json
 from .model import db, User, Role, UserRole, APIKey, create_account, create_role, insert_and_generate_unique_token, delete_token
-from middle_auth_client import auth_required, auth_requires_roles
+# from middle_auth_client import auth_required, auth_requires_roles
 import sqlalchemy
 
 from functools import wraps
@@ -20,15 +20,76 @@ CLIENT_SECRETS_FILE = os.environ['AUTH_OAUTH_SECRET']
 
 SCOPES = ['openid', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
 
+import redis
+
+AUTH_URI = os.environ.get('AUTH_URI', 'localhost:5000/auth')
+
+r = redis.Redis(
+        host=os.environ.get('REDISHOST', 'localhost'),
+        port=int(os.environ.get('REDISPORT', 6379)))
+
+def auth_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if hasattr(flask.g, 'auth_token'):
+            # if authorization header has already been parsed, don't need to re-parse
+            # this allows auth_required to be an optional decorator if auth_requires_role is also used
+            return f(*args, **kwargs)
+
+        if flask.request.environ.get('HTTP_ORIGIN', None) is None:
+            # direct browser access
+            return flask.redirect('https://' + AUTH_URI + '/authorize?redirect=' + flask.request.url, code=302)
+
+        token = flask.request.headers.get('authorization')
+        if not token:
+            resp = flask.Response("Unauthorized", 401)
+            resp.headers['WWW-Authenticate'] = 'Bearer realm="' + AUTH_URI + '"'
+            return resp
+        elif not token.startswith('Bearer '):
+            resp = flask.Response("Invalid Request", 400)
+            resp.headers['WWW-Authenticate'] = 'Bearer realm="' + AUTH_URI + '", error="invalid_request", error_description="Header must begin with \'Bearer\'"'
+            return resp
+        else:
+            token = token.split(' ')[1] # remove schema
+            cached_user_data = r.get("token_" + token)
+
+            if cached_user_data:
+                flask.g.auth_user = json.loads(cached_user_data.decode('utf-8'))
+                flask.g.auth_token = token
+                return f(*args, **kwargs)
+            else:
+                resp = flask.Response("Invalid/Expired Token", 401)
+                resp.headers['WWW-Authenticate'] = 'Bearer realm="' + AUTH_URI + '", error="invalid_token", error_description="Invalid/Expired Token"'
+                return resp
+    return decorated_function
+
+def auth_requires_roles(*required_roles):
+    def decorator(f):
+        @wraps(f)
+        @auth_required
+        def decorated_function(*args, **kwargs):
+            users_roles = flask.g.auth_user['roles']
+            missing_roles = []
+
+            for role in required_roles:
+                if not role in users_roles:
+                    missing_roles += [role]
+
+            if missing_roles:
+                resp = flask.Response("Missing role(s): {0}".format(missing_roles), 403)
+                return resp
+            else:
+                return f(*args, **kwargs)
+
+        return decorated_function
+    return decorator
+
 @mod.route("/version")
 def version():
-    return "neuroglance_auth -- version ddd" + __version__
+    return "neuroglance_auth -- version fff origin: " + flask.request.environ.get('HTTP_ORIGIN', "no origin")
 
 @mod.route("/authorize")
 def authorize():
-    if flask.request.environ['HTTP_ORIGIN'] is None:
-        return flask.Response("Invalid Request", 400)
-
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
         CLIENT_SECRETS_FILE, scopes=SCOPES)
     flow.redirect_uri = flask.url_for('auth.oauth2callback', _external=True, _scheme='https')
@@ -46,11 +107,13 @@ def authorize():
     if not 'redirect' in flask.session:
         return flask.Response("Invalid Request", 400)
 
-    resp = flask.Response(authorization_url)
-    resp.headers['Access-Control-Allow-Credentials'] = 'true'
-    resp.headers['Access-Control-Allow-Origin'] = flask.request.environ['HTTP_ORIGIN']
-
-    return resp
+    if flask.request.environ.get('HTTP_ORIGIN', None) is None:
+        return flask.redirect(authorization_url, code=302)
+    else:
+        resp = flask.Response(authorization_url)
+        resp.headers['Access-Control-Allow-Credentials'] = 'true'
+        resp.headers['Access-Control-Allow-Origin'] = flask.request.environ['HTTP_ORIGIN']
+        return resp
 
 @mod.route("/oauth2callback")
 def oauth2callback():
@@ -84,7 +147,7 @@ def oauth2callback():
     # TODO - detect if there are any differences (username) update the database
 
     if user is None:
-        user = create_account(info, role_names=["edit_all"])
+        user = create_account(info['email'], info['name'], role_names=["edit_all"])
 
     user_json = json.dumps(user.create_cache())
 
