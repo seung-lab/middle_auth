bf33bbc43f3141542cd1f3b78c0abf7b8ab24a29
Chris Jordan <jordanchriss@gmail.com>
2019-06-21 18:05:06 -0400

    returning error if session cookie does not exist indicating that they probably have third-party cookies disabled.

--- neuroglancer_auth/server.py
+++ neuroglancer_auth/server.py
@@ -53,6 +53,9 @@ def authorize():
 
 @mod.route("/oauth2callback")
 def oauth2callback():
+    if not 'session' in flask.request.cookies:
+        return flask.Response("Invalid Request, are third-party cookies enabled?", 400)
+
     if not 'state' in flask.session:
         return flask.Response("Invalid Request", 400)
 
