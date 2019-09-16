9673a7af4d85e6d38bab0ba851f602aef0c3b4da
Chris Jordan <jordanchriss@gmail.com>
2019-09-16 13:31:31 -0400

    Bump version: 0.3.1 → 0.3.2

--- neuroglancer_auth/__init__.py
+++ neuroglancer_auth/__init__.py
@@ -10,7 +10,7 @@ from .model import User, APIKey
 from werkzeug.contrib.fixers import ProxyFix
 import redis # used in the envvar config
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 
 def setup_app():
