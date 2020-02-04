5b82efb63c62ea94d9874d27731d1c2a4c8e4ea3
Chris Jordan <jordanchriss@gmail.com>
2020-02-04 15:48:44 -0500

    added api version to url schema

--- neuroglancer_auth/__init__.py
+++ neuroglancer_auth/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 from flask_session import Session
 from flask_cors import CORS
 
-from .server import mod
+from .server import api_v1, admin_site
 from .model.base import db
 from .model.user import User
 from .model.api_key import APIKey
@@ -28,7 +28,8 @@ def setup_app():
         db.init_app(app)
         db.create_all()
 
-    app.register_blueprint(mod)
+    app.register_blueprint(api_v1)
+    app.register_blueprint(admin_site)
 
     @app.before_first_request
     def initialize():
