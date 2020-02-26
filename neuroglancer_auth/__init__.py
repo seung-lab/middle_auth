292cdd9c247677e3f09548e5c4c5bce5a8da3f4d
Chris Jordan <jordanchriss@gmail.com>
2020-02-26 16:35:16 -0500

    testing mac update

--- neuroglancer_auth/__init__.py
+++ neuroglancer_auth/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 from flask_session import Session
 from flask_cors import CORS
 
-from .server import version_bp, api_v1_bp, admin_site_bp
+from .server import version_bp, api_v1_bp, admin_site_bp, br_bp
 from .model.base import db
 from .model.user import User
 from .model.api_key import APIKey
@@ -16,6 +16,11 @@ import redis # used in the envvar config
 
 __version__ = '1.2.0'
 
+
+DEFAULT_ADMINS = [
+    ["chris@eyewire.org", "Chris Jordan", "seung"],
+    ["sven.dorkenwald@googlemail.com", "Sven Dorkenwald", "seung"]
+]
 
 def setup_app():
     app.config.from_envvar('AUTH_CONFIG_SETTINGS')
@@ -31,14 +36,16 @@ def setup_app():
     app.register_blueprint(version_bp)
     app.register_blueprint(api_v1_bp)
     app.register_blueprint(admin_site_bp)
+    app.register_blueprint(br_bp)
 
     @app.before_first_request
     def initialize():
-        existing_admin = User.get_by_email("chris@eyewire.org")
-        
-        if not existing_admin:
-            User.create_account("chris@eyewire.org", "chris", admin=True, group_names=["default"])
-        
+        for email, name, pi in DEFAULT_ADMINS:
+            existing_user = User.get_by_email(email)
+
+            if not existing_user:
+                User.create_account(email, name, pi, admin=True, group_names=["default"])
+
         APIKey.load_into_cache()
 
     return app
