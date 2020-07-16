a91210867984bbc2e1cfa1f913a1abcdbea2dc62
Chris Jordan <jordanchriss@gmail.com>
2020-07-16 12:38:33 -0400

    added a temporary table and api endpoint to check if a cell is public

--- neuroglancer_auth/server.py
+++ neuroglancer_auth/server.py
@@ -5,7 +5,7 @@ import googleapiclient.discovery
 import urllib
 import uuid
 import json
-from middle_auth_client import auth_required, auth_requires_admin, auth_requires_permission
+from .decs import auth_required, auth_requires_admin, auth_requires_permission
 import sqlalchemy
 from furl import furl
 
@@ -16,6 +16,7 @@ from .model.group import Group
 from .model.user_group import UserGroup
 from .model.dataset import Dataset
 from .model.group_dataset import GroupDataset
+from .model.cell_temp import CellTemp
 
 import os
 
@@ -647,3 +648,8 @@ def get_sa_permissions(sa_id):
         return flask.jsonify(permissions)
     else:
         return flask.Response("Service account doesn't exist", 404)
+
+@api_v1_bp.route('/table/<table_id>/root/<int:root_id>/is_public')
+@auth_required
+def temp_is_root_public(table_id, root_id):
+    return flask.jsonify(CellTemp.is_public(table_id, root_id))
