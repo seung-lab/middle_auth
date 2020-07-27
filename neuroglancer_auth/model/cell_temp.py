99d02bec64575525011ee03c2ad87e3ef826f110
Chris Jordan <jordanchriss@gmail.com>
2020-07-27 11:42:51 -0400

    fixed cell_temp schema

--- neuroglancer_auth/model/cell_temp.py
+++ neuroglancer_auth/model/cell_temp.py
@@ -3,7 +3,7 @@ from .base import db
 class CellTemp(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     table_id = db.Column('table_id', db.String(120), nullable=False)
-    root_id = db.Column('root_id', db.Integer, db.ForeignKey("group.id"), nullable=False)
+    root_id = db.Column('root_id', db.BigInteger, nullable=False)
     public = db.Column(db.Boolean, server_default="0", nullable=False)
     __table_args__ = (db.UniqueConstraint("table_id", "root_id"),)
 
