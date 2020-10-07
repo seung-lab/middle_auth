93bef942411ce7834c14b1657e8a1e3cb8a79668
Chris Jordan <jordanchriss@gmail.com>
2020-10-07 11:19:48 -0400

    added user stats endpoint

--- neuroglancer_auth/model/user.py
+++ neuroglancer_auth/model/user.py
@@ -127,6 +127,21 @@ class User(db.Model):
         return User.query.filter(User.email.ilike(f'%{email}%')).all()
 
     @staticmethod
+    def filter_by_created(from_time=None, to_time=None):
+        res = User.query.filter(User.parent_id.is_(None))
+
+        if from_time is not None:
+            res = res.filter(User.created >= func.to_timestamp(from_time))
+
+        if to_time is not None:
+            res = res.filter(User.created <= func.to_timestamp(to_time))
+
+        try:
+            return res.all()
+        except psycopg2.errors.DatetimeFieldOverflow:
+            return []
+
+    @staticmethod
     def search_by_name(name):
         return User.query.filter(User.parent_id.is_(None)).filter(User.name.ilike(f'%{name}%')).all()
     
