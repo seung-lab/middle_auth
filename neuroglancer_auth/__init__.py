2ee7b6ff74fb2f79415383767487bd67fdfeae54
Chris Jordan <jordanchriss@gmail.com>
2021-07-08 17:50:51 -0400

    Bump version: 2.3.6 → 2.3.7

--- neuroglancer_auth/__init__.py
+++ neuroglancer_auth/__init__.py
@@ -14,7 +14,7 @@ from .model.api_key import APIKey
 from werkzeug.middleware.proxy_fix import ProxyFix
 import redis # used in the envvar config
 
-__version__ = '2.3.6'
+__version__ = '2.3.7'
 
 
 DEFAULT_ADMINS = [
