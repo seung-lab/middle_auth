0ab9e293ad8b4851336c190766bbd57bd9e765f7
Chris Jordan <jordanchriss@gmail.com>
2021-07-09 11:08:59 -0400

    multiple terms of service

--- neuroglancer_auth/model/dataset.py
+++ neuroglancer_auth/model/dataset.py
@@ -3,11 +3,13 @@ from .base import db
 class Dataset(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
+    tos_id = db.Column('tos_id', db.Integer, db.ForeignKey("tos.id"), nullable=True)
 
     def as_dict(self):
         return {
             "id": self.id,
             "name": self.name,
+            "tos_id": self.tos_id,
         }
 
     @staticmethod
@@ -27,3 +29,21 @@ class Dataset(db.Model):
         db.session.add(dataset)
         db.session.commit()
         return dataset
+    
+    def update_cache(self):
+        from .group_dataset_permission import GroupDatasetPermission
+
+        groups = GroupDatasetPermission.get_groups_by_dataset(self.id)
+
+        for group in groups:
+            group.update_cache() # TODO, avoid duplication if user belongs to dataset through multiple groups
+
+    def update(self, data):
+        fields = ['name', 'tos_id']
+
+        for field in fields:
+            if field in data:
+                setattr(self, field, data[field]) #'null'?
+
+        db.session.commit()
+        self.update_cache()
