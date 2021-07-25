4f60dcbaa1e72c5e6a0f66c5d4e5214837ea267f
Chris Jordan <jordanchriss@gmail.com>
2021-07-24 22:21:50 -0400

    token - multiple tokens per user, descriptions, last used time, UI

--- neuroglancer_auth/model/api_key.py
+++ neuroglancer_auth/model/api_key.py
@@ -6,14 +6,22 @@ from sqlalchemy.sql import func
 
 class APIKey(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column('user_id', db.Integer, db.ForeignKey("user.id"), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=True)
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey("user.id"), unique=False, nullable=False)
     key = db.Column(db.String(32), unique=True, nullable=False)
+    created = db.Column(db.DateTime, server_default=func.now())
+    updated = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+    last_used = db.Column(db.DateTime, nullable=True)
 
     def as_dict(self):
         res = {
             "id": self.id,
             "user_id": self.user_id,
             "token": self.key,
+            "description": self.description,
+            "created": self.created,
+            "updated": self.updated,
+            "last_used": self.last_used,
         }
 
         return res
@@ -22,13 +30,17 @@ class APIKey(db.Model):
     def get_by_key(key):
         return APIKey.query.filter_by(key=key).first()
     
+    def update_last_used(self):
+        self.last_used = func.now()
+        db.session.commit()
+    
     @staticmethod
     def get_by_user_id(user_id):
-        return APIKey.query.filter_by(user_id=user_id)
+        return APIKey.query.filter_by(user_id=user_id).order_by(APIKey.id.desc()).all()
 
     @staticmethod
     def get_by_user_id_token_id(user_id, token_id):
-        return APIKey.query.filter_by(user_id=user_id, id=token_id)
+        return APIKey.query.filter_by(user_id=user_id, id=token_id).first()
 
     # load api keys into cache if they don't already exist in redis
     # i.e. new deployment or some redis failure
@@ -44,7 +56,20 @@ class APIKey(db.Model):
                 maybe_insert_token(user.id, api_key.key, json.dumps(user.create_cache()), ex=None)
 
     @staticmethod
-    def generate(user_id):
+    def generate(user_id, description=None):
+        from .user import User
+        user = User.get_by_id(user_id)
+
+        token = user.generate_token()
+
+        entry = APIKey(user_id=user_id, key=token, description=description)
+        db.session.add(entry)
+        db.session.commit()
+
+        return token
+
+    @staticmethod
+    def refresh(user_id): #deprecated
         from .user import User
         user = User.get_by_id(user_id)
 
@@ -116,3 +141,19 @@ def insert_and_generate_unique_token(user_id, value, ex=None):
         token = secrets.token_hex(16)
         if maybe_insert_token(user_id, token, value, ex):
             return token
+
+def get_redis_cache(token):
+    cached_user_data = r.get("token_" + token)
+    if cached_user_data:
+        return json.loads(cached_user_data.decode('utf-8'))
+
+def get_user_id_from_token(token):
+    redis_res = r.get("token_" + token)
+
+    if redis_res:
+        redis_res = json.loads(redis_res.decode('utf-8'))
+        return redis_res['id']
+    else:
+        api_key = APIKey.get_by_key(token)
+        if api_key:
+            return api_key.user_id
