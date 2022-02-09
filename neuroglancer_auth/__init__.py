412b07c35ffa82294656ede98a63ba5b01cf5d80
Chris Jordan <jordanchriss@gmail.com>
2022-02-09 18:47:21 -0500

    Bump version: 2.7.3 → 2.8.0

--- neuroglancer_auth/__init__.py
+++ neuroglancer_auth/__init__.py
@@ -16,7 +16,7 @@ from .model.api_key import APIKey
 from werkzeug.middleware.proxy_fix import ProxyFix
 import redis # used in the envvar config
 
-__version__ = '2.7.3'
+__version__ = '2.8.0'
 
 def setup_app():
     app.config.from_envvar('AUTH_CONFIG_SETTINGS')
