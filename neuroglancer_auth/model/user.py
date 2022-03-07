6a21f92a3c8c847107ace1b9284ee36ff3c66917
Chris Jordan <jordanchriss@gmail.com>
2022-03-07 15:25:17 -0500

    added user associations

--- neuroglancer_auth/model/user.py
+++ neuroglancer_auth/model/user.py
@@ -6,6 +6,8 @@ import json
 from sqlalchemy.sql import func
 import sqlalchemy
 
+from sqlalchemy.orm import relationship
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), unique=False, nullable=False) # public
@@ -16,6 +18,12 @@ class User(db.Model):
     created = db.Column(db.DateTime, server_default=func.now())
     parent_id = db.Column('parent_id', db.Integer, db.ForeignKey("user.id"), nullable=True)
     read_only = db.Column(db.Boolean, server_default="0", nullable=False)
+
+    def __repr__(self):
+        return self.name
+
+    groups = relationship("Group", secondary='user_group', backref=db.backref('users', lazy='dynamic'))
+    associations = relationship("Association", secondary='user_association', backref=db.backref('users', lazy='dynamic'))
 
     def as_dict(self):
         res = {
