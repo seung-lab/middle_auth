9315575ebdc5928bfaccf8e47a78efdb6640cccb
Chris Jordan <jordanchriss@gmail.com>
2022-09-28 14:11:42 -0400

    Bump version: 2.16.0 → 2.16.1

--- neuroglancer_auth/server.py
+++ neuroglancer_auth/server.py
@@ -28,7 +28,7 @@ import os
 
 from functools import wraps
 
-__version__ = '2.16.0'
+__version__ = '2.16.1'
 
 TOKEN_NAME = os.environ.get('TOKEN_NAME', "middle_auth_token")
 URL_PREFIX = os.environ.get('URL_PREFIX', 'auth')
