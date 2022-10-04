8f7ae5d05cdbef05b1543714a2aaad5b6f5d25e1
Chris Jordan <jordanchriss@gmail.com>
2022-10-04 16:42:18 -0400

    auth fix for flask admin

--- neuroglancer_auth/flask_admin.py
+++ neuroglancer_auth/flask_admin.py
@@ -1,7 +1,11 @@
 from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
 from middle_auth_client import auth_required
-from flask import redirect, url_for, g
+import flask
+import json
+import os
+
+from .model.base import r
 from .model.app import App
 from .model.cell_temp import CellTemp
 from .model.user import User
@@ -13,26 +17,51 @@ from .model.dataset import Dataset
 from .model.cell_temp import CellTemp
 from .model.table_mapping import ServiceTable
 
-class SuperAdminView(ModelView):
-   can_export = True
+TOKEN_NAME = os.environ.get('TOKEN_NAME', "middle_auth_token")
 
-   @auth_required
-   def is_accessible(self):
-      return g.auth_user['admin']
-         
-   def inaccessible_callback(self, name, **kwargs):
-      # redirect to login page if user doesn't have access
-      return redirect(url_for('admin.index'))
+def get_user_in_flask():
+    if hasattr(flask.g, 'auth_token'):
+        return
+
+    cookie_name = TOKEN_NAME
+    token = flask.request.cookies.get(cookie_name)
+    auth_header = flask.request.headers.get('authorization')
+    query_param_token = flask.request.args.get(TOKEN_NAME)
+    if query_param_token:
+        token = query_param_token
+    auth_header = flask.request.headers.get('authorization')
+    if auth_header:
+        if not auth_header.startswith('Bearer '):
+            return
+        else:  # auth header takes priority
+            token = auth_header.split(' ')[1]  # remove schema
+    cached_user_data = r.get("token_" + token) if token else None
+    if cached_user_data:
+        return json.loads(cached_user_data.decode('utf-8'))
+    if cached_user_data:
+        flask.g.auth_user = cached_user_data
+        flask.g.auth_token = token
+
+class SuperAdminView(ModelView):
+    can_export = True
+
+    def is_accessible(self):
+        get_user_in_flask()
+        auth_user = flask.g.get('auth_user', None)
+        return auth_user and auth_user['admin']
+
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        return flask.redirect(flask.url_for('admin.index'))
 
 # Create customized index view class that handles login & registration
 class MyAdminIndexView(AdminIndexView):
-     @expose('/', methods=["GET"])
-     @auth_required
-     def index(self):
-          return super(MyAdminIndexView, self).index()
+    @expose('/', methods=["GET"])
+    @auth_required
+    def index(self):
+        return super(MyAdminIndexView, self).index()
 
-     @auth_required
-     def is_accessible(self):
+    def is_accessible(self):
         return True
 
 def setup_admin(app, db):
