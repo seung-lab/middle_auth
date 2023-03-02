7b999e8e4e0b7ecb841bfd69adb76a992efe14de
Chris Jordan <jordanchriss@gmail.com>
2023-03-02 14:01:41 -0500

    Bump version: 2.22.1 → 2.22.2

--- neuroglancer_auth/__init__.py
+++ neuroglancer_auth/__init__.py
@@ -17,7 +17,7 @@ from .flask_admin import setup_admin
 from werkzeug.middleware.proxy_fix import ProxyFix
 import redis # used in the envvar config
 
-__version__ = '2.22.1'
+__version__ = '2.22.2'
 
 def setup_app():
     app.config.from_envvar('AUTH_CONFIG_SETTINGS')
