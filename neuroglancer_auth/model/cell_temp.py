a9fdfe6ab34129a1f1f09fa54430de455fc2593b
Forrest Collman <forrest.collman@gmail.com>
2023-03-02 08:12:13 -0800

    added get_public_roots endpoint

--- neuroglancer_auth/model/cell_temp.py
+++ neuroglancer_auth/model/cell_temp.py
@@ -1,15 +1,18 @@
 from .base import db
 
+
 class CellTemp(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    table_id = db.Column('table_id', db.String(120), nullable=False)
-    root_id = db.Column('root_id', db.BigInteger, nullable=False)
+    table_id = db.Column("table_id", db.String(120), nullable=False)
+    root_id = db.Column("root_id", db.BigInteger, nullable=False)
     public = db.Column(db.Boolean, server_default="0", nullable=False)
     __table_args__ = (db.UniqueConstraint("table_id", "root_id"),)
 
     @staticmethod
     def is_public(table_id, root_id):
-        query = CellTemp.query.filter_by(table_id=table_id, root_id=root_id, public=True).exists()
+        query = CellTemp.query.filter_by(
+            table_id=table_id, root_id=root_id, public=True
+        ).exists()
         return db.session.query(query).scalar()
 
     @staticmethod
@@ -24,3 +27,8 @@ class CellTemp(db.Model):
     def table_has_public(table_id):
         query = CellTemp.query.filter_by(table_id=table_id, public=True).exists()
         return db.session.query(query).scalar()
+
+    @staticmethod
+    def get_public_roots(table_id):
+        query = CellTemp.query.filter_by(table_id=table_id, public=True)
+        return [r.root_id for r in query.all()]
